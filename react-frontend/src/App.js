998749ccfae054cf3afbe0b292d304392d0c7de7
Sandhya Gurrala <51099415+gsandhya2011@users.noreply.github.com>
2022-01-07 11:12:59 -0600

    Add files via upload

---
+++ react-frontend/src/App.js
@@ -0,0 +1,34 @@
+import logo from './logo.svg';
+import './App.css';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import ListEmployeeComponent from './components/ListEmployeeComponent';
+import HeaderComponent from './components/HeaderComponent';
+import FooterComponent from './components/FooterComponent';
+import CreateEmployeeComponent from './components/CreateEmployeeComponent';
+import UpdateEmployeeComponent from './components/UpdateEmployeeComponent';
+import ViewEmployeeComponent from './components/ViewEmployeeComponent';
+
+function App() {
+  return (
+    <div>
+      <Router>
+        <div className="container">
+          <HeaderComponent/>
+            <div className="container">
+                <Switch> 
+                <Route path = "/" exact component={ListEmployeeComponent}></Route>
+                <Route path = "/employees" component={ListEmployeeComponent}></Route> 
+                //step1
+                <Route path = "/add-employee/:id" component={CreateEmployeeComponent}></Route>
+                <Route path = "/view-employee/:id" component={ViewEmployeeComponent}></Route>
+                {/* <Route path = "/update-employee/:id" component={UpdateEmployeeComponent}></Route> */}
+                </Switch> 
+            </div>
+          <FooterComponent />
+        </div>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
